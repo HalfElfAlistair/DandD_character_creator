87509c2a600ebfa2950b72a364f5175ac4786e25
HalfElfAlistair <ajss90@hotmail.co.uk>
2021-03-21 19:33:56 +0000

    added some navigation buttons for the process

--- jquery_processes.js
+++ jquery_processes.js
@@ -1,16 +1,31 @@
 $(document).ready(function() {
 
     $("#character-card").hide();
+    $("#name-section").hide();
 
-    $("#race-list").click(function() {
-        $("#character-card").show();
-    });
-
-    $("#submit-button").click(function() {
+    $(".sub-button").click(function() {
         let raceSelection = $("#race-list option:selected").val();
         let nameSelection = $("#character-name").val();
         $("#race-card").text("Race: " + raceSelection);
         $("#name-card").text("Name: " + nameSelection);
+    });
+
+    $("#next-1").click(function() {
+        $("#race-section").hide();
+        $("#name-section").show();
+    });
+
+    $("#previous-2").click(function() {
+        $("#name-section").hide();
+        $("#race-section").show();
+    });
+
+    $("#show").click(function() {
+        $("#character-card").show();
+    });
+
+    $("#hide").click(function() {
+        $("#character-card").hide();
     });
 
 });
