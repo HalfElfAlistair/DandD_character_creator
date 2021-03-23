23f9853873c20518d462b3e50aae941316f877ca
HalfElfAlistair <ajss90@hotmail.co.uk>
2021-03-23 18:37:14 +0000

    Added jquery to hide/show relevant subrace selectors where required. Also the character card will display the subrace choice when needed.

--- jquery_processes.js
+++ jquery_processes.js
@@ -1,11 +1,42 @@
 $(document).ready(function() {
 
-    $("#character-card").hide();
-    $("#name-section").hide();
+    $("#character-card, #name-section, #subrace, #subrace-dwarf, #subrace-elf, #subrace-gnome, #subrace-halfling").hide();
 
     $("#race-list").change(function() {
         let raceSelection = $("#race-list option:selected").val();
         $("#race-card").text("Race: " + raceSelection);
+
+        if (raceSelection == "Dwarf") {
+            $("#subrace, #subrace-dwarf").show();
+            $("#subrace-elf, #subrace-gnome, #subrace-halfling").hide();
+            $("#dwarf-subrace-list").change(function() {
+                let subraceSelection = $("#dwarf-subrace-list option:selected").val();
+                $("#race-card").text("Race: " + raceSelection + " (Subrace: " + subraceSelection + ")");
+            });
+        } else if (raceSelection == "Elf") {
+            $("#subrace, #subrace-elf").show();
+            $("#subrace-dwarf, #subrace-gnome, #subrace-halfling").hide();
+            $("#elf-subrace-list").change(function() {
+                let subraceSelection = $("#elf-subrace-list option:selected").val();
+                $("#race-card").text("Race: " + raceSelection + " (Subrace: " + subraceSelection + ")");
+            });
+        } else if (raceSelection == "Gnome") {
+            $("#subrace, #subrace-gnome").show();
+            $("#subrace-dwarf, #subrace-elf, #subrace-halfling").hide();
+            $("#gnome-subrace-list").change(function() {
+                let subraceSelection = $("#gnome-subrace-list option:selected").val();
+                $("#race-card").text("Race: " + raceSelection + " (Subrace: " + subraceSelection + ")");
+            });
+        } else if (raceSelection == "Halfling") {
+            $("#subrace, #subrace-halfling").show();
+            $("#subrace-dwarf, #subrace-elf, #subrace-gnome").hide();
+            $("#halfling-subrace-list").change(function() {
+                let subraceSelection = $("#halfling-subrace-list option:selected").val();
+                $("#race-card").text("Race: " + raceSelection + " (Subrace: " + subraceSelection + ")");
+            });
+        } else {
+            $("#subrace").hide();
+        }
     });
 
     $("#character-name").change(function() {
