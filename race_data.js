3be78463e3398ed9673114e8137ca97da1460c22
HalfElfAlistair <ajss90@hotmail.co.uk>
2021-04-03 20:29:01 +0100

    added subraces to race_data object and amended some variable names in jquery_processes

--- race_data.js
+++ race_data.js
@@ -1,29 +1,75 @@
 const races = {
     "Dragonborn": {
         maxAge: 80,
+        subraces: "",
     },
     "Dwarf": {
         maxAge: 350,
+        subraces: {
+            "Hill Dwarf": {
+            },
+            "Mountain Dwarf": {
+            },
+        },
     },
     "Elf": {
         maxAge: 750,
+        subraces: {
+            "High Elf": {
+                baseInches: 54,
+                heightDiceType: 10,
+                weightDiceRolls: 1,
+                weightDiceType: 4,
+                basePounds: 90,
+            },
+            "Wood Elf": {
+                baseInches: 54,
+                heightDiceType: 10,
+                weightDiceRolls: 1,
+                weightDiceType: 4,
+                basePounds: 100,
+            },
+            "Dark Elf": {
+                baseInches: 53,
+                heightDiceType: 6,
+                weightDiceRolls: 1,
+                weightDiceType: 6,
+                basePounds: 75,
+            },
+        },
     },
     "Gnome": {
         maxAge: 499,
+        subraces: {
+            "Forest Gnome": {
+            },
+            "Rock Gnome": {
+            },
+        },
     },
     "Half-Elf": {
         maxAge: 200,
+        subraces: "",
     },
     "Halfling": {
         maxAge: 150,
+        subraces: {
+            "Lightfoot": {
+            },
+            "Stout": {
+            },
+        },
     },
     "Half-Orc": {
         maxAge: 75,
+        subraces: "",
     },
     "Human": {
         maxAge: 99,
+        subraces: "",
     },
     "Tiefling": {
         maxAge: 110,
+        subraces: "",
     },
 };
