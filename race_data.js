cebb21d8d4e42db0e118f10377a061f3bec1fd2e
HalfElfAlistair <ajss90@hotmail.co.uk>
2021-04-05 23:00:17 +0100

    added some data for subraces and values to be used in height and weight roller functions

--- race_data.js
+++ race_data.js
@@ -2,13 +2,28 @@ const races = {
     "Dragonborn": {
         maxAge: 80,
         subraces: "",
+        baseInches: 66,
+        heightDiceType: 8,
+        weightDiceRolls: 2,
+        weightDiceType: 6,
+        basePounds: 175,
     },
     "Dwarf": {
         maxAge: 350,
         subraces: {
             "Hill Dwarf": {
+                baseInches: 44,
+                heightDiceType: 4,
+                weightDiceRolls: 2,
+                weightDiceType: 6,
+                basePounds: 115,
             },
             "Mountain Dwarf": {
+                baseInches: 48,
+                heightDiceType: 4,
+                weightDiceRolls: 2,
+                weightDiceType: 6,
+                basePounds: 130,
             },
         },
     },
@@ -42,34 +57,76 @@ const races = {
         maxAge: 499,
         subraces: {
             "Forest Gnome": {
+                baseInches: 35,
+                heightDiceValue: 4,
+                weightDiceRolls: 1,
+                weightDiceType: 1,
+                basePounds: 35,
             },
             "Rock Gnome": {
+                baseInches: 35,
+                heightDiceValue: 4,
+                weightDiceRolls: 1,
+                weightDiceType: 1,
+                basePounds: 35,
             },
         },
+
     },
     "Half-Elf": {
         maxAge: 200,
         subraces: "",
+        baseInches: 57,
+        heightDiceType: 8,
+        weightDiceRolls: 2,
+        weightDiceType: 4,
+        basePounds: 110,
     },
     "Halfling": {
         maxAge: 150,
         subraces: {
             "Lightfoot": {
+                baseInches: 31,
+                heightDiceValue: 4,
+                weightDiceRolls: 1,
+                weightDiceType: 1,
+                basePounds: 35,
             },
             "Stout": {
+                baseInches: 31,
+                heightDiceValue: 4,
+                weightDiceRolls: 1,
+                weightDiceType: 1,
+                basePounds: 35,
             },
         },
+
     },
     "Half-Orc": {
         maxAge: 75,
         subraces: "",
+        baseInches: 58,
+        heightDiceType: 10,
+        weightDiceRolls: 2,
+        weightDiceType: 6,
+        basePounds: 140,
     },
     "Human": {
         maxAge: 99,
         subraces: "",
+        baseInches: 56,
+        heightDiceType: 10,
+        weightDiceRolls: 2,
+        weightDiceType: 4,
+        basePounds: 110,
     },
     "Tiefling": {
         maxAge: 110,
         subraces: "",
+        baseInches: 57,
+        heightDiceType: 8,
+        weightDiceRolls: 2,
+        weightDiceType: 4,
+        basePounds: 110,
     },
 };
