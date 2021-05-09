ec602d50d17836f4213671a3bf09d844b1eeeff4
HalfElfAlistair <ajss90@hotmail.co.uk>
2021-05-09 20:40:08 +0100

    added the different classes and their respective hit dice values

--- class_data.js
+++ class_data.js
@@ -1,1 +1,38 @@
-const classes = {};
+const classes = {
+    "Barbarian": {
+        hitDiceValue: 12,
+    },
+    "Bard": {
+        hitDiceValue: 8,
+    },
+    "Cleric": {
+        hitDiceValue: 8,
+    },
+    "Druid": {
+        hitDiceValue: 8,
+    },
+    "Fighter": {
+        hitDiceValue: 10,
+    },
+    "Monk": {
+        hitDiceValue: 8,
+    },
+    "Paladin": {
+        hitDiceValue: 10,
+    },
+    "Ranger": {
+        hitDiceValue: 10,
+    },
+    "Rogue": {
+        hitDiceValue: 8,
+    },
+    "Sorcerer": {
+        hitDiceValue: 6,
+    },
+    "Warlock": {
+        hitDiceValue: 8,
+    },
+    "Wizard": {
+        hitDiceValue: 6,
+    },
+};
