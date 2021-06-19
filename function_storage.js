24ed002909b0b68e04e25629eed083d48137d575
HalfElfAlistair <ajss90@hotmail.co.uk>
2021-06-19 16:17:13 +0100

    added function to fill language selector options and print the chosen option to #languages-card

--- function_storage.js
+++ function_storage.js
@@ -37,3 +37,27 @@ function subraceSelectorProcess(subraceObject) {
         $("#subrace-selector").append(`<option class="subrace-option">${subraceObject[s][0]}</option>`);
     }
 }
+
+function languageProcess(languageAllowance, languageDefault, raceSelection) {
+    // Displays the additional language selector if applicable to the race selection
+    if (languageAllowance === 1) {
+        $("#language-section").show();
+
+        // Removes the 'Elvish' option if previously added
+        $("#elvish-language-option").remove();
+
+        // Adds an 'Elvish' option if the selected race is 'Human' (otherwise removing the option).
+        if (raceSelection === "Human") {
+            $("#language-selector").append(`<option id="elvish-language-option">Elvish</option>`);
+        } else {
+            $("#elvish-language-option").remove();
+        }
+
+        // Appends the updated language information to #languages-card
+        $("#language-selector").change(function() {
+            $("#languages-card").text(languageDefault + ", " + $("#language-selector option:selected").val());
+        })
+    } else {
+        $("#language-section").hide();
+    }
+}
