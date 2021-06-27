0fcb8146d6bb309b13af06cd2d6bf378cc61ea9b
HalfElfAlistair <ajss90@hotmail.co.uk>
2021-06-27 12:32:47 +0100

    copied various nav and button code from jquery processes

--- primary_process.js
+++ primary_process.js
@@ -37,4 +37,60 @@ $(document).ready(function() {
 
     });
 
+    // Hides #previous button on load
+    $("#previous").hide();
+
+    // Ensures #previous button is hidden for the first section and #next button is hidden from the last section
+    $("body").click(function() {
+    if ($("#physical-creation section:first").is(":visible")) {
+        $("#previous").hide();
+        $("#next").show();
+    } else if ($("#physical-creation section:last").is(":visible")) {
+        $("#next").hide();
+        $("#previous").show();
+    } else {
+        $("#next").show();
+        $("#previous").show();
+    }
+    })
+
+    // Nav and show/hide card button functions
+    $("#next").click(function() {
+        $("#physical-creation section:visible").next().show().prev().hide();
+    });
+
+    $("#previous").click(function() {
+        $("#physical-creation section:visible").prev().show().next().hide();
+    });
+
+    $("#show-hide-toggle").click(function() {
+        if ($("#show-hide-toggle").text() == "Show Card") {
+            $("#character-card").show();
+            $("#show-hide-toggle").text("Hide Card");
+        } else {
+            $("#character-card").hide();
+            $("#show-hide-toggle").text("Show Card");
+        }
+    })
+
+    $("#show-hide-table").click(function() {
+        if ($("#show-hide-table").text() == "Show Table") {
+            $("#draconic-ancestry-table").show();
+            $("#show-hide-table").text("Hide Table");
+        } else {
+            $("#draconic-ancestry-table").hide();
+            $("#show-hide-table").text("Show Table");
+        }
+    })
+
+    $("#dark-light-toggle").click(function() {
+        if ($("#dark-light-toggle").text() == "Darkvision") {
+            $('link[href="assets/stylesheets/css/daylight_style.css"]').attr("href", "assets/stylesheets/css/darkvision_style.css");
+            $("#dark-light-toggle").text("Daylight");
+        } else {
+            $('link[href="assets/stylesheets/css/darkvision_style.css"]').attr("href", "assets/stylesheets/css/daylight_style.css");
+            $("#dark-light-toggle").text("Darkvision");
+        }
+    })
+
 });
